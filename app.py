ee43b9fe4302dbea0fb0428d2029f0d822e82a39
affan00733 <affanmohhd@gmail.com>
2023-07-05 00:43:18 +0530

    changes

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
 import os 
 import evadb
-from dotenv.main import load_dotenv
 
-load_dotenv()
-
-if os.path.exists(".env") and os.environ.get("OPENAI_KEY") is not None:
-    user_api_key = os.environ["OPENAI_KEY"]
-else:
-    user_api_key = "KEY"
-os.environ["OPENAI_KEY"] = user_api_key
-open_ai_key = os.environ.get('OPENAI_KEY')
-
+os.environ["OPENAI_KEY"] = "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"
+print(os.environ["OPENAI_KEY"])
 cursor = evadb.connect().cursor()
 cursor.drop_table("MyPDF").execute()
 cursor.load(file_regex="./layout-parser-paper.pdf",
             format="pdf", table_name="MyPDF").execute()
 df = cursor.table("MyPDF").df()
 print(df)
-cursor.drop_udf("ChatGPT").df()
-cursor.create_udf("ChatGPT",True,"./chatgpt.py").df()
-text_noise = "noise text"
-data = (
-    cursor.table("MyPDF")
-    .filter("page < 2")
-    .cross_apply(f"ChatGPT('Act as a classifier. Classify this text as noise:',{text_noise})","objs(response)")
-).df()
-print(data)
+#cursor.drop_function("ChatGPT").df()
+#cursor.create_function("ChatGPT",True,"./chatgpt.py").df()
+
+question = "Act as a text classification model that classifies paragraphs extracted from research papers. Classify front-matter, reference citations, and table of contents as 'noise'. Classify the body of the paper as 'signal'.  Classify this text fragment:"
+column = "text"
+prompt = "Return only one word in your response 'noise' or 'signal'."
+
+import pandas as pd
+pd.set_option('display.max_colwidth', None)
+
+output = cursor.table("MyPDF").filter("page > 2 AND page < 4").select(f"""ChatGPT("{question}", data, "{prompt}")""").df()
+
+output.to_csv("output.csv")
+
+print(output)
